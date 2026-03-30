8c3fc71174a738fe3dfdd575e5b2e05de90ec9b8
Nirmal <zeninfosolution7@gmail.com>
2026-03-30 09:05:33 +0530

    Nextconfig.ts file changed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/ads.txt",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "text/plain",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
